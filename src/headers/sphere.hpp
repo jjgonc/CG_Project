b3b23828bc2ea40ff1dd0ba39c104be301823207
jjgonc <josejoao_2001@hotmail.com>
2022-03-06 02:36:11 +0000

    box working

--- src/headers/sphere.hpp
+++ src/headers/sphere.hpp
@@ -1,4 +1,18 @@
 #ifndef CG_PROJECT_SPHERE_HPP
 #define CG_PROJECT_SPHERE_HPP
 
+class Sphere
+{
+private:
+    float radius;
+    int slices;
+    int stacks;
+
+public:
+    Sphere(float radius, int slices, int stacks);
+    // std::vector<Point> Sphere::pointsGenerator();
+};
+
+
+
 #endif //CG_PROJECT_SPHERE_HPP
--- src/src/box.cpp
+++ src/src/box.cpp
@@ -8,6 +8,128 @@ Box::Box(float e, int g)
     grid = g;
 }
 
+
+
+
+
+vector<Point> Box::pointsGenerator()
+{
+    vector<Point> points;
+
+    float subEdge = edge / grid;
+    float halfEdge = edge/2.0;   //ignorar pq nao estamos a centrar na origem
+
+    for (int i = 0; i < grid; i++)
+    {
+        for (int j = 0; j < grid; j++)
+        {
+
+            // face 1 - virada para a frente
+            Point p1_frente = Point(-halfEdge + i * subEdge, -halfEdge + j * subEdge, halfEdge);
+            Point p2_frente = Point(-halfEdge + (i+1) * subEdge, -halfEdge + j * subEdge, halfEdge);
+            Point p3_frente = Point(-halfEdge + (i + 1) * subEdge, -halfEdge + (j+1) * subEdge, halfEdge);
+            Point p4_frente = Point(-halfEdge + i * subEdge, -halfEdge + (j+1) * subEdge, halfEdge);
+
+            points.push_back(p1_frente);
+            points.push_back(p2_frente);
+            points.push_back(p4_frente);
+
+            points.push_back(p2_frente);
+            points.push_back(p3_frente);
+            points.push_back(p4_frente);
+
+
+
+            // face 2 - virada para a esquerda
+            Point p1_lado1 = Point(-halfEdge, -halfEdge + i * subEdge, -halfEdge + j * subEdge);
+            Point p2_lado1 = Point(-halfEdge, -halfEdge + (i+1) * subEdge, -halfEdge + j * subEdge);
+            Point p3_lado1 = Point(-halfEdge, -halfEdge + (i+1) * subEdge, -halfEdge + (j+1) * subEdge);
+            Point p4_lado1 = Point(-halfEdge, -halfEdge + i * subEdge, -halfEdge + (j+1) * subEdge);
+
+            points.push_back(p1_lado1);
+            points.push_back(p4_lado1);
+            points.push_back(p2_lado1);
+
+            points.push_back(p2_lado1);
+            points.push_back(p4_lado1);
+            points.push_back(p3_lado1);
+
+
+            // face 3 - virada para a direita
+            Point p1_lado2 = Point(halfEdge, -halfEdge + i * subEdge, -halfEdge + j * subEdge);
+            Point p2_lado2 = Point(halfEdge, -halfEdge + (i+1) * subEdge, -halfEdge + j * subEdge);
+            Point p3_lado2 = Point(halfEdge, -halfEdge + (i+1) * subEdge, -halfEdge + (j+1) * subEdge);
+            Point p4_lado2 = Point(halfEdge, -halfEdge + i * subEdge, -halfEdge + (j+1) * subEdge);
+            
+            points.push_back(p1_lado2);
+            points.push_back(p2_lado2);
+            points.push_back(p4_lado2);
+
+            points.push_back(p2_lado2);
+            points.push_back(p3_lado2);
+            points.push_back(p4_lado2);
+            
+
+            // face 4 - virada para trás
+            Point p1_tras = Point(-halfEdge + i * subEdge, -halfEdge + j * subEdge, -halfEdge);
+            Point p2_tras = Point(-halfEdge + (i+1) * subEdge, -halfEdge + j * subEdge, -halfEdge);
+            Point p3_tras = Point(-halfEdge + (i + 1) * subEdge, -halfEdge + (j+1) * subEdge, -halfEdge);
+            Point p4_tras = Point(-halfEdge + i * subEdge, -halfEdge + (j+1) * subEdge, -halfEdge);
+            
+            points.push_back(p1_tras);
+            points.push_back(p4_tras);
+            points.push_back(p2_tras);
+            
+            points.push_back(p2_tras);
+            points.push_back(p4_tras);
+            points.push_back(p3_tras);
+
+
+            // face 5 - virada para cima
+            Point p1_cima = Point(-halfEdge + i * subEdge, halfEdge, -halfEdge + j * subEdge);
+            Point p2_cima = Point(-halfEdge + (i+1) * subEdge, halfEdge, -halfEdge + j * subEdge);
+            Point p3_cima = Point(-halfEdge + (i+1) * subEdge, halfEdge, -halfEdge + (j+1) * subEdge);
+            Point p4_cima = Point(-halfEdge + i * subEdge, halfEdge, -halfEdge + (j+1) * subEdge);
+
+            points.push_back(p1_cima);
+            points.push_back(p4_cima);
+            points.push_back(p2_cima);
+
+            points.push_back(p2_cima);
+            points.push_back(p4_cima);
+            points.push_back(p3_cima);
+
+
+            // face 6 - virada para baixo
+            Point p1_baixo = Point(-halfEdge + i * subEdge, -halfEdge, -halfEdge + j * subEdge);
+            Point p2_baixo = Point(-halfEdge + (i+1) * subEdge, -halfEdge, -halfEdge + j * subEdge);
+            Point p3_baixo = Point(-halfEdge + (i+1) * subEdge, -halfEdge, -halfEdge + (j+1) * subEdge);
+            Point p4_baixo = Point(-halfEdge + i * subEdge, -halfEdge, -halfEdge + (j+1) * subEdge);
+
+            points.push_back(p1_baixo);
+            points.push_back(p2_baixo);
+            points.push_back(p4_baixo);
+
+            points.push_back(p2_baixo);
+            points.push_back(p3_baixo);
+            points.push_back(p4_baixo);
+        }
+    }
+
+    return points;
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
 vector<Point> Box::pointsGenerator()
 {
     vector<Point> points;
@@ -91,3 +213,5 @@ vector<Point> Box::pointsGenerator()
 
     return points;
 }
+
+*/
