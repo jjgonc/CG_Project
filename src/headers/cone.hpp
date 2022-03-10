7b091706509f3358f60e9a2d19f9d82d91591fcd
jjgonc <josejoao_2001@hotmail.com>
2022-03-10 09:09:50 +0000

    primeiros passos no cone

--- src/headers/cone.hpp
+++ src/headers/cone.hpp
@@ -1,4 +1,19 @@
 #ifndef CG_PROJECT_CONE_HPP
 #define CG_PROJECT_CONE_HPP
 
+
+class Cone
+{
+private:
+    float radius;
+    float height;
+    int slices;
+    int stacks;
+
+public:
+    Cone(float radius, float height, int slices, int stacks);
+    std::vector<Point> drawCone(double radius, double height, int slices, int stacks);
+};
+
+
 #endif //CG_PROJECT_CONE_HPP
