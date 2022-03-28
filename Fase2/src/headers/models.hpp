0b2852bace4cd2dd6a22a26273af54e4b263a362
devSaraiva <bernardosaraiva1503@gmail.com>
2022-03-28 23:31:38 +0100

    models iniciado

--- Fase2/src/headers/models.hpp
+++ Fase2/src/headers/models.hpp
@@ -1,4 +1,29 @@
 #ifndef CG_PROJECT_MODELS_HPP
 #define CG_PROJECT_MODELS_HPP
 
-#endif //CG_PROJECT_MODELS_HPP
+#include <vector>
+#include "point.hpp"
+
+using namespace std;
+
+class Coordinate
+{
+public:
+    float x, y, z, angle;
+};
+
+class Figure
+{
+public:
+    vector<Point> pontos;
+};
+
+class Models
+{
+public:
+    vector<Models> groups;
+    Coordinate rotate, translate, scale;
+    vector<Figure> figures;
+};
+
+#endif // CG_PROJECT_MODELS_HPP
